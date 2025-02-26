f98ec5b1b2fa141762ef29fb5b5df718d6738628
jaygreentree <jgreentreevin@gmail.com>
2025-02-26 12:38:27 -0600

    dev

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'welcome']);
 
 
 Route::get('/counter', Counter::class);
+
+Route::get('/page-name', [HomeController::class, 'page'])->name('page');
