21cc1af411444c98fc417fe428c9fd3c7a97c562
jaygreentree <jgreentreevin@gmail.com>
2025-02-26 08:01:17 -0600

    testing

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <h1>{{ $count }}</h1>
+ 
+    <button class="btn btn-secondary" wire:click="increment">+</button>
+ 
+    <button class="btn btn-secondary" wire:click="decrement">-</button>
+</div>
